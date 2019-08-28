80f9d5584c92afe3e7dcdd4a12c66dabe5ef91dc
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-08-28 11:03:25 +0200

    Projet  ServiceLabs

--- ServiceLabs/app/src/main/java/com/doranco/servicelabs/MainActivity.java
+++ ServiceLabs/app/src/main/java/com/doranco/servicelabs/MainActivity.java
@@ -3,12 +3,25 @@ package com.doranco.servicelabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.btn1);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
---
+++ ServiceLabs/app/src/main/java/com/doranco/services/MyService.java
@@ -0,0 +1,32 @@
+package com.doranco.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.doranco.servicelabs.R;
+
+public class MyService extends Service {
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public MyService() {
+        super();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, getApplicationContext().getString(R.string.starting_service), Toast.LENGTH_SHORT).show();
+        return START_STICKY; // Dire a android de redemarrer le service quand il aura suffisamment de mémoire
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
