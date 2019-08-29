ba1d565545aec779c288d4364554fb341f8a08c7
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-08-29 11:30:47 +0200

    onUpgrade() et ajout new user in DB

--- SQLiteLabs/app/src/main/java/com/doranco/db/DbHandler.java
+++ SQLiteLabs/app/src/main/java/com/doranco/db/DbHandler.java
@@ -1,11 +1,14 @@
 package com.doranco.db;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DbHandler extends SQLiteOpenHelper {
 
+    private static final String TAG = DbHandler.class.getSimpleName();
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "userdb";
     private static final String TABLE_USERS = "userdetails";
@@ -31,6 +34,29 @@ public class DbHandler extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // delete old table
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        // create the table again
+        onCreate(sqLiteDatabase);
+    }
+
+    // *** CRUD (Create, read, Update, Delete)
+
+    //Ajout d'un nouveau user
+    public void insertUserDetails(String name, String location, String designation){
+        // Récupèrer le repository en écriture
+        SQLiteDatabase db  = this.getWritableDatabase();
+
+        // Insérer les données en utilisant une Map(ContentValues)
+        ContentValues cValues = new ContentValues();
+        cValues.put(KEY_NAME, name);
+        cValues.put(KEY_LOC, location);
+        cValues.put(KEY_DESG, designation);
+
+        // Insérer les données
+        long newRowId = db.insert(TABLE_USERS, null, cValues);
+        Log.d(TAG, "New user added with primary key " + newRowId);
+        db.close();
 
     }
 }
